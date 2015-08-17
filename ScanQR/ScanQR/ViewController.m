3f1e8a33f3bf138de69a49b308784b71a32b59e4
hmeng22 <hmeng22@uwo.ca>
2015-08-16 23:19:36 -0400

    23:19 Aug 16th

--- ScanQR/ScanQR/ViewController.m
+++ ScanQR/ScanQR/ViewController.m
@@ -7,8 +7,20 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
-@interface ViewController ()
+@interface ViewController () <AVCaptureVideoDataOutputSampleBufferDelegate>
+
+@property (weak, nonatomic) IBOutlet UILabel *frameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *messageLabel;
+
+@property (retain) AVCaptureDevice *devices;
+@property (retain) AVCaptureSession *session;
+
+@property (retain) AVCaptureDeviceInput *camera;
+@property (retain) AVCaptureVideoDataOutput *video;
+
+@property (retain) AVCaptureVideoPreviewLayer *previewLayer;
 
 @end
 
@@ -17,11 +29,48 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self.frameLabel.layer setBorderWidth:1.0f];
+    [self.frameLabel.layer setBorderColor:[UIColor redColor].CGColor];
+    
+    [self.messageLabel setText:@"This is a Message Label"];
+    
+    NSError *error;
+    _devices = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    _session = [[AVCaptureSession alloc] init];
+    [_session beginConfiguration];
+    if ([_session canSetSessionPreset:AVCaptureSessionPresetHigh]) {
+        [_session setSessionPreset:AVCaptureSessionPresetHigh];
+    }
+    [_session commitConfiguration];
+    
+    _camera = [AVCaptureDeviceInput deviceInputWithDevice:_devices error:&error];
+    if ([_session canAddInput:_camera]) {
+        [_session beginConfiguration];
+        [_session addInput:_camera];
+        [_session commitConfiguration];
+    }
+    _video = [[AVCaptureVideoDataOutput alloc] init];
+    [_video setVideoSettings:[NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithInt:kCVPixelFormatType_32BGRA],kCVPixelBufferPixelFormatTypeKey,nil]];
+    [_video setAlwaysDiscardsLateVideoFrames:YES];
+    [_video setSampleBufferDelegate:self queue:dispatch_queue_create("video queue", nil)];
+    if ([_session canAddOutput:_video]) {
+        [_session addOutput:_video];
+    }
+    
+    _previewLayer = [AVCaptureVideoPreviewLayer layerWithSession:_session];
+    [_previewLayer setFrame:self.view.bounds];
+    [_previewLayer setVideoGravity:AVLayerVideoGravityResizeAspect];
+    [self.view.layer insertSublayer:_previewLayer atIndex:0];
+    
+    [_session startRunning];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+int x=0;
+- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection
+{
+    x++;
+    [_messageLabel setText:[NSString stringWithFormat:@"%d", x]];
 }
 
 @end
