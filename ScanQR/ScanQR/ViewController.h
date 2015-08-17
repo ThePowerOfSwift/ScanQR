795dd9e62d752bf2f67ffe68eb41848ec6bfbb9b
hmeng22 <hmeng22@uwo.ca>
2015-08-16 22:39:14 -0400

    Aug 16th

---
+++ ScanQR/ScanQR/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScanQR
+//
+//  Created by MengHua on 8/16/15.
+//  Copyright (c) 2015 menghua.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ScanQR/ScanQR/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ScanQR
+//
+//  Created by MengHua on 8/16/15.
+//  Copyright (c) 2015 menghua.cn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
