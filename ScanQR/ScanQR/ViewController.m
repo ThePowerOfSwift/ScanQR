972e19f774cd184c0fbb302fd7ab4163deb84e46
hmeng22 <hmeng22@uwo.ca>
2015-08-19 22:42:09 -0400

    Wed Aug 19th 22:41

--- ScanQR/ScanQR/ViewController.m
+++ ScanQR/ScanQR/ViewController.m
@@ -67,10 +67,30 @@
 }
 
 int x=0;
+- (IBAction)tapAction:(UITapGestureRecognizer *)sender {
+    x++;
+    [self.messageLabel setText:[NSString stringWithFormat:@"Tap %d times.",x]];
+}
+
 - (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection
 {
-    x++;
-    [_messageLabel setText:[NSString stringWithFormat:@"%d", x]];
+    CVImageBufferRef imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
+    CVPixelBufferLockBaseAddress(imageBuffer, 0);
+    void *baseAddress = CVPixelBufferGetBaseAddress(imageBuffer);
+    size_t bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer);
+    size_t width = CVPixelBufferGetWidth(imageBuffer);
+    size_t height = CVPixelBufferGetHeight(imageBuffer);
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    CGContextRef context = CGBitmapContextCreate(baseAddress, width, height, 8, bytesPerRow, colorSpace, kCGBitmapByteOrder32Little |kCGImageAlphaPremultipliedFirst);
+    CGImageRef quartzImage = CGBitmapContextCreateImage(context);
+    CVPixelBufferUnlockBaseAddress(imageBuffer,0);
+    CGContextRelease(context);
+    CGColorSpaceRelease(colorSpace);
+    
+    UIImage *image = [UIImage imageWithCGImage:quartzImage];
+    CGImageRelease(quartzImage);
+    
+    // using UIImage instance image after ...
 }
 
 @end
